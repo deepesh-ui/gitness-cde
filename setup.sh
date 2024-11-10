8c4daaf42d7298b5e63ce113a172cfd9a351c810
Deepesh <deepesh.kumar@harness.io>
2024-11-10 16:35:37 +0530

    chore: updated devcontainer

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt install protobuf-compiler
+sudo apt install golang-go
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28.1
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2.0
+sudo apt install nodejs
+sudo apt install npm
+sudo npm install --global yarn
