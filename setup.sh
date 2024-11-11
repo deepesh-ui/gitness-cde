6a66c15c4eadc731f6c92ca6f230c8de7d2e38df
Deepesh Kumar <81554936+deepesh-ui@users.noreply.github.com>
2024-11-11 08:23:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 sudo apt update
-sudo apt install protobuf-compiler
-sudo apt install golang-go
+sudo apt install -y protobuf-compiler
+sudo apt install  -y golang-go
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28.1
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2.0
-sudo apt install nodejs
-sudo apt install npm
-sudo npm install --global yarn
+sudo apt  -y install nodejs
+sudo apt  -y install npm
+sudo npm  -y install --global yarn
